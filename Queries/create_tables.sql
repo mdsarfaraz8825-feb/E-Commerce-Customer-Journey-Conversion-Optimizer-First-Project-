90ed1e17979525eb8d921fd1d5f6913506eddb8f
Md Sarfaraz <mdsarfaraz882519@gmail.com>
2026-07-08 00:12:23 +0530

    Update create_tables.sql

--- Queries/create_tables.sql
+++ Queries/create_tables.sql
@@ -12,3 +12,32 @@ CREATE TABLE Customer_Behavior (
     subcategory varchar(255),
     product varchar(255)
     );
+
+
+create table products
+(
+  product_id bigint primary key,
+  category_id bigint,
+  category_code varchar(255),
+  brand varchar(255),
+  price decimal(10,2),
+  category varchar(255),
+  subcategory varchar(255),
+  product varchar(255)
+);
+
+
+create table users (
+    user_id bigint primary key
+    );
+
+
+
+CREATE TABLE events (
+   event_id int auto_increment primary key,
+   event_time timestamp,
+   event_type varchar(50),
+   product_id bigint,
+   user_id bigint,
+   user_session varchar(255)
+   );
